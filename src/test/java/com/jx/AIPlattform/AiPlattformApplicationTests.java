1e379e08704a5010839ac65b8acd3c0e8b9228ab
DeepStream <jx2446715265@gmail.com>
2026-03-18 15:04:14 +0800

    后端初始化

---
+++ src/test/java/com/jx/AIPlattform/AiPlattformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx.AIPlattform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlattformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
