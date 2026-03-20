169dbb955e1ecf24df9c305959987f7d30e7f320
DeepStream <jx2446715265@gmail.com>
2026-03-20 20:53:47 +0800

    后端通过自动代码生成完成了通用类的开发

---
+++ src/main/java/com/jx/AIPlatform/generator/MybatisCodeGenerator.java
@@ -0,0 +1,86 @@
+package com.jx.AIPlatform.generator;
+
+import cn.hutool.core.lang.Dict;
+import cn.hutool.setting.yaml.YamlUtil;
+import com.mybatisflex.codegen.Generator;
+import com.mybatisflex.codegen.config.ColumnConfig;
+import com.mybatisflex.codegen.config.GlobalConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.Map;
+
+public class MybatisCodeGenerator {
+
+//    要生成的表名
+    public static final String[] TABEL_NAMES = {"user"};
+
+    public static void main(String[] args) {
+        //配置数据源
+        Dict dict = YamlUtil.loadByPath("application.yaml");
+        Map<String, Object> dataSourceConfig = dict.getByPath("spring.datasource");
+        String url = String.valueOf(dataSourceConfig.get("url"));
+        String username = String.valueOf(dataSourceConfig.get("username"));
+        String password = String.valueOf(dataSourceConfig.get("password"));
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        //创建配置内容
+        GlobalConfig globalConfig = createGlobalConfig();
+
+        //通过 datasource 和 globalConfig 创建代码生成器
+        Generator generator = new Generator(dataSource, globalConfig);
+
+        //生成代码
+        generator.generate();
+    }
+
+    public static GlobalConfig createGlobalConfig() {
+        //创建配置内容
+        GlobalConfig globalConfig = new GlobalConfig();
+
+        //设置根包，生成到临时目录下
+        globalConfig.getPackageConfig()
+                .setBasePackage("com.jx.AIPlatform.genresult");
+
+        //设置表前缀和只生成哪些表，setGenerateTable 未配置时，生成所有表
+        globalConfig.getStrategyConfig()
+                .setGenerateTable(TABEL_NAMES)
+                // 设置逻辑删除的默认字段名称
+                .setLogicDeleteColumn("isDelete");
+
+
+        //设置生成 entity 并启用 Lombok
+        globalConfig.enableEntity()
+                .setWithLombok(true)
+                .setJdkVersion(21);
+
+        //设置生成 mapper
+        globalConfig.enableMapper();
+        globalConfig.enableMapperXml();
+
+        //设置生成 service
+        globalConfig.enableService();
+        globalConfig.enableServiceImpl();
+
+        //设置生成 controller
+        globalConfig.enableController();
+
+        //设置生成注释 比如时间、作者等
+        globalConfig.getJavadocConfig()
+                .setAuthor("JX")
+                .setSince("");
+
+        //可以单独配置某个列
+        ColumnConfig columnConfig = new ColumnConfig();
+        columnConfig.setColumnName("tenant_id");
+        columnConfig.setLarge(true);
+        columnConfig.setVersion(true);
+        globalConfig.getStrategyConfig()
+                .setColumnConfig("tb_account", columnConfig);
+
+        return globalConfig;
+    }
+}
+
