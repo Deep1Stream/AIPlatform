b9bbd562843904b3ec753e939fc77c8442b51b9b
DeepStream <jx2446715265@gmail.com>
2026-03-26 19:47:21 +0800

    后端用户注册业务开发

---
+++ src/main/java/com/jx/AIPlatform/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.jx.AIPlatform.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.jx.AIPlatform.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author JX
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
