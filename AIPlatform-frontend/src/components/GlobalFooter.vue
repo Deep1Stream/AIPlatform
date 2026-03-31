70d1560e0f643ff562e89ba7d82ae4da564e1ab6
DeepStream <jx2446715265@gmail.com>
2026-03-31 20:31:48 +0800

    前端用户登录界面开发

--- AIPlatform-frontend/src/components/GlobalFooter.vue
+++ AIPlatform-frontend/src/components/GlobalFooter.vue
@@ -3,12 +3,12 @@
     <div class="footer-content">
       <p class="copyright">
         <a
-          href="https://www.codefather.cn"
+          href="https://github.com/Yennerfer18/AIPlatform"
           target="_blank"
           rel="noopener noreferrer"
           class="author-link"
         >
-          编程导航原创项目 by 程序员鱼皮
+          DeepSteamAI零代码生成平台项目
         </a>
       </p>
     </div>
