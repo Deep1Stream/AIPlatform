904932f103586ec186e5b00eb817313ba1949bd0
DeepStream <jx2446715265@gmail.com>
2026-04-10 15:50:45 +0800

    应用模块(完成后端及其部署）

--- src/main/java/com/jx/AIPlatform/service/AppService.java
+++ src/main/java/com/jx/AIPlatform/service/AppService.java
@@ -1,10 +1,12 @@
 package com.jx.AIPlatform.service;
 
 import com.jx.AIPlatform.model.dto.app.AppQueryRequest;
+import com.jx.AIPlatform.model.entity.User;
 import com.jx.AIPlatform.model.vo.AppVO;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.jx.AIPlatform.model.entity.App;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -35,4 +37,22 @@ public interface AppService extends IService<App> {
      * @return
      */
     List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 通过对话生成应用代码
+     *
+     * @param appId
+     * @param message 提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+    /**
+     * 应用部署
+     * @param appId 应用ID
+     * @param loginUser 登录用户
+     * @return 可访问的部署地址
+     */
+    String deployApp(Long appId, User loginUser);
 }
